793b458f71a4ea6801166b792db8d4c67e047a42
Michael Krog <michaelryankrog@gmail.com>
2018-12-06 12:09:32 -0700

    day two of coding and having a blast! continued adding javascript. Specifically cleaning code and making more functions. Last got to generating a number between two numbers

--- main.js
+++ main.js
@@ -1,60 +1,131 @@
+// ********************************************
+// VARIABLES **********************************
+// ********************************************
 
-document.addEventListener('DOMContentLoaded', function () {
- function setRange(){
+// Random Generated Number
+var randomNumber = null;
 
- }
-});
-
-
-// Set Range Javascript
-var minRange = document.querySelector('#min-range');
-var maxRange = document.querySelector('#max-range');
+//Initialzed Click Variables
 var setRangeBtn = document.querySelector('#set-range');
-
-
-
-// Guess Range Javascript
-var minGuess = document.querySelector('#min-guess');
-var maxGuess = document.querySelector('#max-guess');
-
-// Clicked the updated button
-setRangeBtn.addEventListener('click', function() {
-    // alert if min and max haven't been set or aren't numbers
-
-    //grab values from both min and max input boxes in seperate variables
-    var minRangeInput = minRange.value;
-    var maxRangeInput = maxRange.value;
-
-    // Change text of current range to min and max values
-    minGuess.innerText = minRangeInput;
-    maxGuess.innerText = maxRangeInput;
-
-    //testing
-    console.log('sup dudes');
-    alert(minRangeInput);
-    alert(maxRangeInput);
-});
-
-//Submit guess Click
 var submitGuessBtn = document.querySelector('#submit-btn');
-var nameOne = document.querySelector('#name-one');
-var nameTwo = document.querySelector('#name-two');
+var resetBtn = document.querySelector('#reset-btn');
+var clearBtn = document.querySelector('#clear-btn');
+var deleteBtn = document.querySelector('.delete-btn'); //Will Have To Update
 
-var challengerOne = document.querySelector('.challenger-one');
-var challengerTwo = document.querySelector('.challenger-two');
+// Initialzed Set Range Variables
+var minRangeInput = document.querySelector('#min-range-input');
+var maxRangeInput = document.querySelector('#max-range-input');
+
+var minRangeOutput = document.querySelector('#min-range-output');
+var maxRangeOutput = document.querySelector('#max-range-output');
+
+
+// Initialzed Guess Range Variables
+var guessOne = document.querySelector('#guess-one'); // Challenger One Guess Input Box
+var guessTwo = document.querySelector('#guess-two'); // Challenger Two Guess Input Box
+var nameOne = document.querySelector('#name-one'); // Name Input Box Challenger One
+var nameTwo = document.querySelector('#name-two'); // Name Input Box Challenger Two
+
+var challengerOne = document.querySelectorAll('.challenger-one'); // Will have to do something
+var challengerTwo = document.querySelectorAll('.challenger-two'); // Will have to do something
+
+// Initialzed Latest Score Variables
+var lastGuessOne = document.querySelector('#last-guess-one'); // Show Challenger One guess field
+var lastGuessTwo = document.querySelector('#last-guess-two'); // Show Challenger Two guess field
+
+// ********************************************
+// EVENT LISTENERS ****************************
+// ********************************************
+setRangeBtn.addEventListener('click', updateClick);
+submitGuessBtn.addEventListener('click', submitClick);
+resetBtn.addEventListener('click', resetClick);
+clearBtn.addEventListener('click', clearClick);
+deleteBtn.addEventListener('click', deleteClick);
+
+// ********************************************
+// FUNCTIONS **********************************
+// ********************************************
+
+// Clicked the UPDATE button
+function updateClick(){
+  //grab values from both min and max input boxes and make them numbers
+  var maxRange = parseInt(maxRangeInput.value);
+  var minRange = parseInt(minRangeInput.value);
+
+  changeRange(maxRange, minRange);
+  generateNumber(maxRange, minRange);
+
+  console.log('Click Update');
+}
+function changeRange(max, min){
+  // Change text of current range to min and max values
+  minRangeOutput.innerText = min;
+  maxRangeOutput.innerText = max;
+}
+function generateNumber(max, min){
+  randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+  console.log(randomNumber);
+}
+
+// Clicked the SUBMIT button
+function submitClick(){
+  // Change Name Taken From Inputs
+  changeName();
+  changeGuess();
+
+  console.log('Click Submit');
+}
+// Change Names Across Page on SUBMIT Click
+function changeName(){
+  for (i = 0; i < challengerOne.length; i++) {
+    challengerOne[i].innerText = nameOne.value;
+  }
+  for (i = 0; i < challengerTwo.length; i++) {
+    challengerTwo[i].innerText = nameTwo.value;
+  }
+}
+// Change Guess Across Page on SUBMIT Click
+function changeGuess(){
+  lastGuessOne.innerText = guessOne.value;
+  lastGuessTwo.innerText = guessTwo.value;
+}
+
+
+
+// Clicked the RESET button
+function resetClick(){
+  console.log('Clicked Reset');
+}
+
+// Clicked the CLEAR button
+function clearClick(){
+  console.log('Clicked Clear');
+}
+
+// Clicked the CLEAR button
+function deleteClick(){
+  console.log('Clicked Delete');
+}
+
+
+
 
 submitGuessBtn.addEventListener('click', function() {
-  var nameOneInput = nameOne.value;
-  var nameTwoInput = nameTwo.value;
-
-  challengerOne.innerText = nameOneInput;
-  challengerTwo.innerText = nameTwoInput;
+  challengerOne.innerText = parseInt(nameOne.value);
+  challengerTwo.innerText = parseInt(nameTwo.value);
+})
 
 
-  console.log(nameOneInput + nameTwoInput)
+submitGuessBtn.addEventListener('click', function() {
+  lastGuessOne.innerText = guessOne.value;
+  lastGuessTwo.innerText = guessTwo.value;
 
-  alert("heya")
-});
+})
+
+
+
+
+
 
 
 
@@ -94,4 +165,12 @@ submitGuessBtn.addEventListener('click', function() {
 // document.querySelector("h1").addEventListener('click', function() {
 
 //   console.log('anchor');
+// });
+
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//  function setRange(){
+//
+//  }
 // });
